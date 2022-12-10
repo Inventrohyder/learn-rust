6788dfa3040325ef48a09a9815acb3badca80af8
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2022-12-10 05:31:25 -0500

    🎉 Initial Commit

---
+++ welcome.rs
@@ -0,0 +1,6 @@
+// My first lines of Rust as coded in the playground
+// https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=14235b93976a7dd994d59693dceb7d30
+
+fn main() {
+    println!("Welcome to Rust!");
+}
